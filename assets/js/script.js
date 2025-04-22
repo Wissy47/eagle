c8314edd3eb375329e963e0313378957e2209030
wisdom ighofose <wissy47@gmail.com>
2025-04-22 23:19:27 +0100

    feat: :sparkles: Added the css and js. file

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+//jquery onload
+
+jQuery(document).ready(function($) {
+    $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 0,
+      navText: [],
+      center: true,
+      autoplay: true,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        1000: {
+          items: 3
+        }
+      }
+    });
+});
