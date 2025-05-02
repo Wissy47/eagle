4c00fb4329be293fe6aa7e9144ba238e8ec254ea
wisdom ighofose <wissy47@gmail.com>
2025-05-02 17:08:53 +0100

    feat: :sparkles: added page archive and single.php file to attend to different output

--- archive.php
+++ archive.php
@@ -3,6 +3,12 @@
 <section class="who_section layout_padding">
     <div class="container">
         <div class="row">
+            
+            <?php if (have_posts()):
+                while (have_posts()):
+                    the_post();
+                    ?>
+
             <div class="col-md-5">
                 <div class="img-box">
                     <?= the_post_thumbnail("full") ?>
@@ -10,27 +16,22 @@
                 </div>
             </div>
             <div class="col-md-7">
-                <?php if (have_posts()):
-                    while (have_posts()):
-                        the_post();
-                        ?>
+               
                         <div class="detail-box">
                             <div class="heading_container">
                                 <h2><?php the_title(); ?> </h2>
-                                <p><?php the_content(); ?></p>
-
-                            <?php endwhile;
-                else: ?>
-                            <p>!Sorry no posts here</p>
-                        <?php endif; ?>
+                            <?php endwhile;?>
                     </div>
-                    <!-- <div>
-              <a href="">
+                    <div>
+              <a href="<?=the_permalink()?>">
                 Read More
               </a>
-            </div> -->
+            </div>
                 </div>
             </div>
+            <?php else: ?>
+                <p>!Sorry no posts here</p>
+            <?php endif; ?>
         </div>
     </div>
 </section>
--- page.php
+++ page.php
@@ -1,10 +1,44 @@
 <?php get_header()?>
+<?php get_header() ?>
+</div>
+<section class="who_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5">
+          <div class="img-box">
+            <?=the_post_thumbnail("full")?>
+            <!-- <img src="/assets/images/who-img.jpg" alt=""> -->
+          </div>
+        </div>
+        <div class="col-md-7">
+            <?php if (have_posts()):
+                while (have_posts()):
+                    the_post(); 
+            ?>
+          <div class="detail-box">
+          <div class="heading_container">
+                <h2><?php the_title(); ?> </h2>
+                <p><?php the_content(); ?></p>
+                        
+                <?php endwhile;
+                else: ?>
+                <p>!Sorry no posts here</p>
+                    <?php endif; ?>
+            </div>
+            <!-- <div>
+              <a href="">
+                Read More
+              </a>
+            </div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end who section -->
 
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-      the_content();
-      endwhile; else: ?>
-      <p>!Sorry no posts here</p>
-<?php endif; ?>
-
-<?php get_footer(); ?>
+  
+    
+    <?php get_footer() ?>
